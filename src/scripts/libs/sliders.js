2fb1de06022b498f00b854c8a2af4b9d548e3925
Yatseniuk Vladyslav <yasenukv@gmail.com>
2025-05-19 17:31:16 +0300

    19.05_17:31

--- src/scripts/libs/sliders.js
+++ src/scripts/libs/sliders.js
@@ -107,8 +107,8 @@ function initSliders() {
 			modules: [Navigation, Pagination],
 			observer: true,
 			observeParents: true,
-			slidesPerView: "auto",
-			spaceBetween: 20,
+			slidesPerView: 1,
+			spaceBetween: 8,
 			//autoHeight: true,
 			speed: 800,
 
@@ -138,28 +138,20 @@ function initSliders() {
 				prevEl: ".swiper-button-prev",
 				nextEl: ".swiper-button-next",
 			},
-			/*
+
 			// Брейкпоінти
 			breakpoints: {
-				640: {
+				340: {
 					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
 				},
-				768: {
+				480: {
 					slidesPerView: 2,
-					spaceBetween: 20,
 				},
 				992: {
-					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
-				},
+					slidesPerView: "auto",
+				}
 			},
-			*/
+
 			// Події
 			on: {},
 		});
